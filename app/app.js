aaa50ad1da6eb4a0419e64f46985a77c2bd1dfe0
VINOD K. SINGH <vksingh@us.ibm.com>
2016-12-14 21:02:17 -0500

    update app.js file

--- app/app.js
+++ app/app.js
@@ -5,6 +5,6 @@ var express = require('express');
 		res.send('Hello World!');
 	});
 	
-	app.listen(3000, function () {
-  		console.log('Example app listening on port 3000!');
+	app.listen(80, function () {
+  		console.log('Example app listening on port 80!');
 	})
